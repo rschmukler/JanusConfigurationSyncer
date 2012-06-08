984dd42e44d155a9cec1e4e98d69c19f94def5c4
Ryan Schmukler <rschmukler@gmail.com>
2012-06-08 15:25:16 -0500

    Fixed escape chars

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ fi
 
 curl -Lo- "http://bit.ly/jcs-rakefile" > $HOME/.janus/Rakefile;
 cd $HOME/.janus;
-echo "Janus Config Syncer Install Complete\nRun:\n\trake install_create - if you'd like to base your config off of the locally installed configs\n\trake install_copy - if you'd like to use the configs on your git"
+echo -e "Janus Config Syncer Install Complete\nSwitch to ~/.janus/ and run:\n\trake install_create - if you'd like to base your config off of the locally installed configs\n\trake install_copy - if you'd like to use the configs on your git"
